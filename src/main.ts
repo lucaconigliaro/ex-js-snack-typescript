5dd53526695259b847d117223907511b912269d2
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-04-23 17:16:30 +0200

    Snack 3 + Bonus

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,23 @@ type Dipendente = {
   readonly emailAziendale: string,
   contratto: "indeterminato" | "determinato" | "freelance"
 };
+
+// 🏆 Snack 3
+type Developer = Dipendente & {
+  livelloEsperienza: "Junior" | "Mid" | "Senior",
+  linguaggi?: string[],
+  certificazioni: string[]
+};
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null,
+  budgetGestito?: number,
+  stakeholderPrincipali: string[]
+};
+
+type Team = {
+  nome: string,
+  progettoAttuale: string | null,
+  budget: number, 
+  membri: [ProjectManager, Developer, ...Developer[]] 
+};
