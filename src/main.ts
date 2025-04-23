aff30bcc429715ca82f1442ded06f3a97bedaa1d
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-04-23 17:14:39 +0200

    Snack 1 + Bonus

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// 🏆 Snack 1
+let data: unknown;
+if (typeof data === "string") {
+  console.log(data.toUpperCase());
+} else if (typeof data === "number") {
+  console.log(data * 2);
+} else if (typeof data === "boolean") {
+  console.log(data ? "Si" : "No");
+} else if ( data === null) {
+  console.log("Il dato è vuoto");
+} else if( Array.isArray(data)) {
+  console.log(data.length);
+} else if (data instanceof Promise) {
+  data.then(v => console.log(v));
+} else {
+  console.log("Tipo non supportato");
+};
